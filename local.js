0a95af58ba5a3ad7f9aa00f43190f06d409d6e1d
somdaterra <somdaterra3@gmail.com>
2025-05-06 11:28:59 -0300

    simplify project

--- local.js
+++
@@ -1,8 +0,0 @@
-// local.js
-const app = require('./api/render-zpl');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
